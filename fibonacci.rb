108428abe12a21bee163169c99b7fed8fa538aa5
sanemat <o.gata.ken@gmail.com>
2010-09-06 02:03:17 +0900

    rename stored_number to restore_number

--- fibonacci.rb
+++ fibonacci.rb
@@ -4,19 +4,19 @@ class Fibonacci
     @store = {}
   end
   def number(input)
-    if number_stored? input then return stored_number input end
+    if number_stored? input then return restore_number input end
     if input == 0 then store_number(input, 0)
     elsif input == 1 then store_number(input, 1)
     else
       store_number(input, number(input - 2) + number(input - 1))
     end
-    stored_number input
+    restore_number input
   end
-  def stored_number(input)
+  def restore_number(input)
     @store[input]
   end
   def number_stored?(input)
-    !stored_number(input).nil?
+    !restore_number(input).nil?
   end
   def store_number(key, value)
     @store[key] = value
