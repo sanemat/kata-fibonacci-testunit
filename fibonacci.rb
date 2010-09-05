290b5dde609186ab0b213f55d47ae33d5f807908
sanemat <o.gata.ken@gmail.com>
2010-09-06 02:01:04 +0900

    refactaring

--- fibonacci.rb
+++ fibonacci.rb
@@ -4,13 +4,22 @@ class Fibonacci
     @store = {}
   end
   def number(input)
-    unless @store[input].nil? then return @store[input] end
-    if input == 0 then @store[input] = 0
-    elsif input == 1 then @store[input] = 1
+    if number_stored? input then return stored_number input end
+    if input == 0 then store_number(input, 0)
+    elsif input == 1 then store_number(input, 1)
     else
-      @store[input] = number(input - 2) + number(input - 1)
+      store_number(input, number(input - 2) + number(input - 1))
     end
+    stored_number input
+  end
+  def stored_number(input)
     @store[input]
+  end
+  def number_stored?(input)
+    !stored_number(input).nil?
+  end
+  def store_number(key, value)
+    @store[key] = value
   end
 end
 
