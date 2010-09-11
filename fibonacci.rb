7b837ee5926286eb39807cc1b468145fcbc7ab5c
sanemat <o.gata.ken@gmail.com>
2010-09-12 01:35:21 +0900

    use ActiveSupport::Memoizable

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,25 +1,14 @@
 # coding: utf-8
+require 'active_support/memoizable'
 class Fibonacci
-  def initialize
-    @store = {}
-  end
+  extend ActiveSupport::Memoizable
   def number(input)
-    if number_stored? input then return restore_number input end
-    if input == 0 then store_number(input, 0)
-    elsif input == 1 then store_number(input, 1)
+    if input == 0 then 0
+    elsif input == 1 then 1
     else
-      store_number(input, number(input - 2) + number(input - 1))
+      number(input - 2) + number(input - 1)
     end
-    restore_number input
   end
-  def restore_number(input)
-    @store[input]
-  end
-  def number_stored?(input)
-    !restore_number(input).nil?
-  end
-  def store_number(key, value)
-    @store[key] = value
-  end
+  memoize :number
 end
 
